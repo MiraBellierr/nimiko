8a67cbf796ecbcac7c9552f5a63057655474e3ac
MiraBellerr <75858@siswa.unimas.my>
2022-09-13 10:10:16 +0800

    add suggest command

---
+++ src/commands/general/suggest.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+module.exports = {
+	name: "suggest",
+	description: "Suggest anything so server can be good",
+	category: "general",
+	run: async (client, message, args) => {
+		if (!args.length)
+			return message.channel.send(
+				"Please include your suggestions with the command."
+			);
+
+		const suggestChannel = await client.channels.fetch("1017088619791527966");
+		const suggestions = args.join(" ");
+
+		message.delete();
+
+		const embed = new Discord.EmbedBuilder()
+			.setAuthor({
+				name: message.author.username,
+				iconURL: message.author.displayAvatarURL(),
+			})
+			.setFields([
+				{
+					name: "Suggestions",
+					value: suggestions,
+				},
+			])
+			.setTimestamp();
+
+		const m = await suggestChannel.send({ embeds: [embed] });
+
+		m.react("✅");
+		m.react("❎");
+
+		message.channel.send(
+			`Hi ${message.author}, thank you for your suggestions ^-^`
+		);
+	},
+};
