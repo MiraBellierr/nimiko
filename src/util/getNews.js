e9a36659860e53b3945176e0b9481512396882a7
Mirabellier <mirabellier3@gmail.com>
2022-09-29 17:14:30 +0800

    handle the error

--- src/util/getNews.js
+++ src/util/getNews.js
@@ -9,40 +9,47 @@ async function getAllNews(client) {
 	});
 	const page = await browser.newPage();
 	await page.goto("https://www.animenewsnetwork.com/");
-	await page.click("span.topics:nth-child(6) > span:nth-child(1)");
-	const html = await page.content();
-	const $ = cheerio.load(html);
-	const news = [];
 
-	$("div.mainfeed-day:nth-child(3) > div > div.news:not(.is-filtered)").each(
-		(i, el) => {
-			const title = `${$(el).find("div.wrap > div > h3 > a").text()}`;
-			const link = `https://www.animenewsnetwork.com/${$(el)
-				.find("a")
-				.attr("href")}`;
-			const img = `https://www.animenewsnetwork.com/${$(el)
-				.find("div.thumbnail")
-				.attr("data-src")}`;
-			const desc = `${$(el)
-				.find("div.wrap > div > div.preview")
-				.text()}`.trim();
-			const date = $(el)
-				.find("div.wrap > div > div.byline > time")
-				.attr("datetime");
+	try {
+		await page.click("span.topics:nth-child(6) > span:nth-child(1)");
+		const html = await page.content();
+		const $ = cheerio.load(html);
+		const news = [];
 
-			news.push({
-				title,
-				link,
-				img,
-				desc,
-				date,
-			});
-		}
-	);
+		$("div.mainfeed-day:nth-child(3) > div > div.news:not(.is-filtered)").each(
+			(i, el) => {
+				const title = `${$(el).find("div.wrap > div > h3 > a").text()}`;
+				const link = `https://www.animenewsnetwork.com/${$(el)
+					.find("a")
+					.attr("href")}`;
+				const img = `https://www.animenewsnetwork.com/${$(el)
+					.find("div.thumbnail")
+					.attr("data-src")}`;
+				const desc = `${$(el)
+					.find("div.wrap > div > div.preview")
+					.text()}`.trim();
+				const date = $(el)
+					.find("div.wrap > div > div.byline > time")
+					.attr("datetime");
 
-	await browser.close();
+				news.push({
+					title,
+					link,
+					img,
+					desc,
+					date,
+				});
+			}
+		);
 
-	return news;
+		await browser.close();
+
+		return news;
+	} catch (e) {
+		await browser.close();
+
+		return [];
+	}
 }
 
 async function getNews(client) {
@@ -52,82 +59,90 @@ async function getNews(client) {
 	});
 	const page = await browser.newPage();
 	await page.goto("https://www.animenewsnetwork.com/");
-	await page.click("span.topics:nth-child(6) > span:nth-child(1)");
-	const html = await page.content();
-	const $ = cheerio.load(html);
-	const news = [];
 
-	$("div.mainfeed-day:nth-child(3) > div > div.news:not(.is-filtered)").each(
-		(i, el) => {
-			const title = `${$(el).find("div.wrap > div > h3 > a").text()}`;
-			const link = `https://www.animenewsnetwork.com/${$(el)
-				.find("a")
-				.attr("href")}`;
-			const img = `https://www.animenewsnetwork.com/${$(el)
-				.find("div.thumbnail")
-				.attr("data-src")}`;
-			const desc = `${$(el)
-				.find("div.wrap > div > div.preview")
-				.text()}`.trim();
-			const date = $(el)
-				.find("div.wrap > div > div.byline > time")
-				.attr("datetime");
+	try {
+		await page.click("span.topics:nth-child(6) > span:nth-child(1)");
+		const html = await page.content();
+		const $ = cheerio.load(html);
+		const news = [];
 
-			news.push({
-				title,
-				link,
-				img,
-				desc,
-				date,
-			});
-		}
-	);
+		$("div.mainfeed-day:nth-child(3) > div > div.news:not(.is-filtered)").each(
+			(i, el) => {
+				const title = `${$(el).find("div.wrap > div > h3 > a").text()}`;
+				const link = `https://www.animenewsnetwork.com/${$(el)
+					.find("a")
+					.attr("href")}`;
+				const img = `https://www.animenewsnetwork.com/${$(el)
+					.find("div.thumbnail")
+					.attr("data-src")}`;
+				const desc = `${$(el)
+					.find("div.wrap > div > div.preview")
+					.text()}`.trim();
+				const date = $(el)
+					.find("div.wrap > div > div.byline > time")
+					.attr("datetime");
 
-	if (news.length < 1) {
-		await browser.close();
-
-		return console.log("Checking for new Anime news... no news yet");
-	}
-
-	let previousNews = fs.readFileSync(
-		"./src/database/json/news.json",
-		"utf8",
-		(err, data) => {
-			if (err) console.log(err);
-
-			return data;
-		}
-	);
-
-	previousNews = JSON.parse(previousNews);
-
-	if (!previousNews.count) previousNews.count = 0;
-	if (!previousNews.title) previousNews.title = null;
-
-	console.log(
-		`Checking for new Anime news... (${news[0].title}) - latest? - ${
-			previousNews.count === news.length && news[0].title === previousNews.title
-				? "yes"
-				: "no, updating..."
-		}`
-	);
-
-	if (
-		previousNews.count !== news.length &&
-		news[0].title !== previousNews.title
-	) {
-		fs.writeFile(
-			"./src/database/json/news.json",
-			JSON.stringify({ count: news.length, title: news[0].title }),
-			(err) => {
-				if (err) console.log(err);
+				news.push({
+					title,
+					link,
+					img,
+					desc,
+					date,
+				});
 			}
 		);
 
-		client.emit("animeNews", news[0]);
-	}
+		if (news.length < 1) {
+			await browser.close();
 
-	await browser.close();
+			return console.log("Checking for new Anime news... no news yet");
+		}
+
+		let previousNews = fs.readFileSync(
+			"./src/database/json/news.json",
+			"utf8",
+			(err, data) => {
+				if (err) console.log(err);
+
+				return data;
+			}
+		);
+
+		previousNews = JSON.parse(previousNews);
+
+		if (!previousNews.count) previousNews.count = 0;
+		if (!previousNews.title) previousNews.title = null;
+
+		console.log(
+			`Checking for new Anime news... (${news[0].title}) - latest? - ${
+				previousNews.count === news.length &&
+				news[0].title === previousNews.title
+					? "yes"
+					: "no, updating..."
+			}`
+		);
+
+		if (
+			previousNews.count !== news.length &&
+			news[0].title !== previousNews.title
+		) {
+			fs.writeFile(
+				"./src/database/json/news.json",
+				JSON.stringify({ count: news.length, title: news[0].title }),
+				(err) => {
+					if (err) console.log(err);
+				}
+			);
+
+			client.emit("animeNews", news[0]);
+		}
+
+		await browser.close();
+	} catch (e) {
+		await browser.close();
+
+		console.log(e);
+	}
 }
 
 async function getHoloNews(client) {
@@ -137,63 +152,69 @@ async function getHoloNews(client) {
 	});
 	const page = await browser.newPage();
 
-	await page.goto("https://twitter.com/hololive_En", {
-		waitUntil: "networkidle2",
-	});
-
-	await page.waitForSelector("article[data-testid='tweet']");
-
-	const html = await page.content();
-	const $ = cheerio.load(html);
-
-	const URLs = [];
-
-	$("article[data-testid='tweet']")
-		.find("a")
-		.each((i, el) => {
-			if ($(el).attr("href").includes("/status/")) {
-				const url = `https://twitter.com${$(el).attr("href")}`;
-
-				URLs.push(url);
-			}
+	try {
+		await page.goto("https://twitter.com/hololive_En", {
+			waitUntil: "networkidle2",
 		});
 
-	const url = URLs[0];
+		await page.waitForSelector("article[data-testid='tweet']");
 
-	const splitUrl = url.split("/");
-	const id = splitUrl[splitUrl.length - 1].split("?")[0];
+		const html = await page.content();
+		const $ = cheerio.load(html);
 
-	let previousNews = fs.readFileSync(
-		"./src/database/json/holo.json",
-		"utf8",
-		(err, data) => {
-			if (err) console.log(err);
+		const URLs = [];
 
-			return data;
-		}
-	);
+		$("article[data-testid='tweet']")
+			.find("a")
+			.each((i, el) => {
+				if ($(el).attr("href").includes("/status/")) {
+					const url = `https://twitter.com${$(el).attr("href")}`;
 
-	previousNews = JSON.parse(previousNews);
+					URLs.push(url);
+				}
+			});
 
-	console.log(
-		`Checking for new HoloNews... (${id}) - latest? - ${
-			id === previousNews.id ? "yes" : "no, updating..."
-		}`
-	);
+		const url = URLs[0];
 
-	if (previousNews.id !== id) {
-		fs.writeFile(
+		const splitUrl = url.split("/");
+		const id = splitUrl[splitUrl.length - 1].split("?")[0];
+
+		let previousNews = fs.readFileSync(
 			"./src/database/json/holo.json",
-			JSON.stringify({ id }),
-			(err) => {
+			"utf8",
+			(err, data) => {
 				if (err) console.log(err);
+
+				return data;
 			}
 		);
 
-		client.emit("holoTweet", url);
-	}
+		previousNews = JSON.parse(previousNews);
 
-	await browser.close();
+		console.log(
+			`Checking for new HoloNews... (${id}) - latest? - ${
+				id === previousNews.id ? "yes" : "no, updating..."
+			}`
+		);
+
+		if (previousNews.id !== id) {
+			fs.writeFile(
+				"./src/database/json/holo.json",
+				JSON.stringify({ id }),
+				(err) => {
+					if (err) console.log(err);
+				}
+			);
+
+			client.emit("holoTweet", url);
+		}
+
+		await browser.close();
+	} catch (e) {
+		await browser.close();
+
+		console.log(e);
+	}
 }
 
 module.exports = { getAllNews, getNews, getHoloNews };
